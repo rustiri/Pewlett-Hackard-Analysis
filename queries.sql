e963c7a085b8dfa9f86802bdafbc7f1cd9ea3100
Rita O'Rourke <rustiri@yahoo.com>
2020-10-26 14:52:47 -0700

    adding retirement_info.csv file and updating queries.sql schema.sql

--- queries.sql
+++ queries.sql
@@ -9,3 +9,22 @@ SELECT * FROM salaries;
 SELECT * FROM dept_emp;
 
 SELECT * FROM titles;
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Retirement eligibility: will create new table called retirement_info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
